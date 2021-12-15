b4c791bae3f041d6d08facf0ce20385e02fdda24
Derryk Boyd <derryk@redmantech.com>
2021-12-14 18:25:52 -0700

    Add a prominent warning to the staging site

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/Header.css';
 
 export default function Header(props) {
+  const [modalOpen, setModalOpen] = React.useState(process.env.REACT_APP_ENV === 'staging');
   const signOut = () => {
     props.firebaseApp.auth().signOut();
     localStorage.clear();
@@ -17,7 +18,26 @@ export default function Header(props) {
           </button>
         )}
       </header>
-      {process.env.REACT_APP_ENV === 'staging' && <h1>** STAGING SITE **</h1>}
+      {process.env.REACT_APP_ENV === 'staging' && (
+        <>
+          <h1>** STAGING SITE **</h1>
+          {modalOpen && (
+            <div class="staging-modal">
+              <h2>This is the staging site for testing only.</h2>
+              <p>
+                If you are taking stats for premier league go to the{' '}
+                <a href="https://afc-stats.netlify.app/">Live Site</a>
+              </p>
+              <a href="https://afc-stats.netlify.app/">
+                <button className="btn">Go to live site</button>
+              </a>
+              <button className="btn btn-del" onClick={() => setModalOpen(false)}>
+                Continue to test
+              </button>
+            </div>
+          )}
+        </>
+      )}
     </>
   );
 }
